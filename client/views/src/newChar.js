13f2b01d0755b74b59f493776243dc587191ce7a
jc3 <jc3@jc3.aura>
2020-05-05 20:52:46 -0700

    turned chars into buttons to work on post route

--- client/views/src/newChar.js
+++ client/views/src/newChar.js
@@ -1,15 +1,57 @@
 var chars = document.getElementById('newChar')
-for(var i in chars.children){
-    chars.children[i].onmouseover = hoverBox;
-    chars.children[i].onmouseleave = removeBox;
+//"src","img/megaman.png"
+//"src","img/mario.png"
+//"src","img/whitemage.png"
+var available = ["mario","megaman","whitemage"]
+//<input type="checkbox" id="charSel1" style="display: ">
+//<label for="charSel1"><img src="img/megaman.png"></label>
+
+//create form and event handlers
+for(var i in available){
+    var div = document.createElement("div")
+    var lab = document.createElement("label");
+    lab.setAttribute("for", ""+available[i]+"")
+    var input = document.createElement("input");
+    var img = document.createElement("img")
+    img.setAttribute("src","img/"+available[i]+".png")
+    img.setAttribute("class","baseborder")
+    input.setAttribute("type","checkbox");
+    input.setAttribute("name", available[i])
+    input.setAttribute("id", available[i])
+    input.setAttribute("style", "display: none; padding: 15px")
+    //img.setAttribute("class","charSel");
+    img.onclick = stickBox;
+    img.onmouseover = hoverBox;
+    img.onmouseleave = removeBox;
+    lab.appendChild(input);
+    lab.appendChild(img);
+    //lab.append(available[i])
+    div.appendChild(lab);
+    chars.appendChild(div);
 }
 
-//console.log(chars);
+function setVal(){
+
+}
 
 function hoverBox(){
-    this.setAttribute("class","border")
+    if(!this.licked){
+        this.setAttribute("class","border")
+    }
 }
 
 function removeBox(){
-    this.setAttribute("class","")
+    if(!this.licked){
+        this.setAttribute("class","baseborder")
+    }
+}
+
+function stickBox(){
+    if(!this.licked){
+        this.setAttribute("class","border")
+        this.licked = true
+    } else {
+        this.setAttribute("class","baseborder")
+        this.licked = false;
+    }
 }
